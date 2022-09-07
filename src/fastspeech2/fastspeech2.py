added deepspeed, fixed depthwise convolution paddddding, added layer drop, fixed mixed precision loss

src/fastspeech2/fastspeech2.py
@@ -3,12 +3,17 @@ import multiprocessing
 from pathlib import Path
 import random
 import string
+import hashlib
+import json
+import pickle
+from copy import copy
 
 import pytorch_lightning as pl
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from torch.nn.modules.transformer import TransformerEncoder, TransformerEncoderLayer
+from .torch_transformer import TransformerEncoder
+from torch.nn.modules.transformer import TransformerEncoderLayer
 import wandb
 import pandas as pd
 import numpy as np
@@ -44,6 +49,7 @@ class FastSpeech2(pl.LightningModule):
         min_length=0.5,
         max_length=32,
         augment_duration=0.1,  # 0.1,
+        layer_dropout=0.1,
         variances=["pitch", "energy", "snr"],
         variance_levels=["frame", "frame", "frame"],
         variance_transforms=["cwt", "none", "none"],  # "cwt", "log", "none"
@@ -99,6 +105,7 @@ class FastSpeech2(pl.LightningModule):
         tf_linear_schedule_end=20,
         tf_linear_schedule_end_ratio=0.0,
         num_workers=num_cpus,
+        cache_path=None,
     ):
         super().__init__()
 
@@ -133,6 +140,8 @@ class FastSpeech2(pl.LightningModule):
             ]
         )
 
+        self.eval_log_data = None
+
         # data
         if train_ds is not None:
             if train_ds_kwargs is None:
@@ -150,13 +159,47 @@ class FastSpeech2(pl.LightningModule):
             train_ds_kwargs["n_fft"] = n_fft
             train_ds_kwargs["win_length"] = win_length
             train_ds_kwargs["hop_length"] = hop_length
-            self.train_ds = TTSDataset(train_ds, **train_ds_kwargs)
+            if cache_path is not None:
+                if isinstance(train_ds, list):
+                    hashes = [x.hash for x in train_ds]
+                else:
+                    hashes = [train_ds.hash]
+                kwargs = copy(train_ds_kwargs)
+                kwargs.update({"hashes": hashes})
+                ds_hash = hashlib.md5(json.dumps(kwargs, sort_keys=True).encode('utf-8')).hexdigest()
+                cache_path_full = Path(cache_path) / f"train-full-{ds_hash}.pt"
+                if cache_path_full.exists():
+                    with cache_path_full.open("rb") as f:
+                        self.train_ds = pickle.load(f)
+                else:
+                    self.train_ds = TTSDataset(train_ds, **train_ds_kwargs)
+                    self.train_ds.hash = ds_hash
+                    with open(cache_path_full, "wb") as f:
+                        pickle.dump(self.train_ds, f)
+            else:
+                self.train_ds = TTSDataset(train_ds, **train_ds_kwargs)
         if valid_ds is not None:
             if valid_ds_kwargs is None:
                 valid_ds_kwargs = {}
-            self.valid_ds = self.train_ds.create_validation_dataset(
-                valid_ds, **valid_ds_kwargs
-            )
+            if cache_path is not None:
+                kwargs = copy(valid_ds_kwargs)
+                kwargs.update({"hashes": [self.train_ds.hash, valid_ds.hash]})
+                ds_hash = hashlib.md5(json.dumps(kwargs, sort_keys=True).encode('utf-8')).hexdigest()
+                cache_path_full = Path(cache_path) / f"valid-full-{ds_hash}.pt"
+                if cache_path_full.exists():
+                    with cache_path_full.open("rb") as f:
+                        self.valid_ds = pickle.load(f)
+                else:
+                    self.valid_ds = self.train_ds.create_validation_dataset(
+                        valid_ds, **valid_ds_kwargs
+                    )
+                    self.valid_ds.hash = ds_hash
+                    with open(cache_path_full, "wb") as f:
+                        pickle.dump(self.valid_ds, f)
+            else:
+                self.valid_ds = self.train_ds.create_validation_dataset(
+                    valid_ds, **valid_ds_kwargs
+                )
 
         self.synth = Synthesiser(device=self.device)
 
@@ -188,6 +231,7 @@ class FastSpeech2(pl.LightningModule):
                 conv_depthwise=self.hparams.encoder_depthwise_conv,
             ),
             num_layers=self.hparams.encoder_layers,
+            layer_drop_prob=self.hparams.layer_dropout,
         )
 
         if self.hparams.encoder_conformer:
@@ -255,6 +299,7 @@ class FastSpeech2(pl.LightningModule):
                 conv_depthwise=self.hparams.encoder_depthwise_conv,
             ),
             num_layers=self.hparams.decoder_layers,
+            layer_drop_prob=self.hparams.layer_dropout,
         )
         if self.hparams.decoder_conformer:
             if self.hparams.decoder_dim_feedforward is not None:
@@ -833,6 +878,7 @@ class FastSpeech2(pl.LightningModule):
         parser.add_argument("--min_length", type=float, default=0.5)
         parser.add_argument("--max_length", type=float, default=32)
         parser.add_argument("--augment_duration", type=float, default=0.1)
+        parser.add_argument("--layer_dropout", type=float, default=0.1)
         parser.add_argument(
             "--variances", nargs="+", type=str, default=["pitch", "energy", "snr"]
         )

src/fastspeech2/loss.py
@@ -38,6 +38,7 @@ class FastSpeech2Loss(nn.Module):
         return loss(pred, truth)
 
     def forward(self, result, target, frozen_components=[]):
+
         variances_pred = {var: result[f"variances_{var}"] for var in self.variances}
         variances_target = {
             var: target[f"variances_{var}"]
@@ -77,8 +78,8 @@ class FastSpeech2Loss(nn.Module):
                 if transform == "cwt":
                     losses[variance + "_cwt"] = (
                         FastSpeech2Loss.get_loss(
-                            variances_pred[variance].float(),
-                            variances_target[variance].float(),
+                            variances_pred[variance],
+                            variances_target[variance].to(dtype=result["mel"].dtype),
                             self.l1_loss,
                             variance_mask,
                             unsqueeze=True,
@@ -87,27 +88,25 @@ class FastSpeech2Loss(nn.Module):
                     )
                     losses[variance + "_mean"] = (
                         self.mse_loss(
-                            result[f"variances_{variance}"]["mean"].float(),
+                            result[f"variances_{variance}"]["mean"],
                             torch.tensor(target[f"variances_{variance}_mean"])
-                            .to(result[f"variances_{variance}"]["mean"].device)
-                            .float(),
+                            .to(result[f"variances_{variance}"]["mean"].device, dtype=result["mel"].dtype),
                         )
                         * self.loss_alphas[variance]
                     )
                     losses[variance + "_std"] = (
                         self.mse_loss(
-                            result[f"variances_{variance}"]["std"].float(),
+                            result[f"variances_{variance}"]["std"],
                             torch.tensor(target[f"variances_{variance}_std"])
-                            .to(result[f"variances_{variance}"]["std"].device)
-                            .float(),
+                            .to(result[f"variances_{variance}"]["std"].device, dtype=result["mel"].dtype),
                         )
                         * self.loss_alphas[variance]
                     )
                 else:
                     losses[variance] = (
                         FastSpeech2Loss.get_loss(
-                            variances_pred[variance].float(),
-                            variances_target[variance].float(),
+                            variances_pred[variance],
+                            variances_target[variance].to(dtype=result["mel"].dtype),
                             self.mse_loss,
                             variance_mask,
                         )
@@ -118,7 +117,7 @@ class FastSpeech2Loss(nn.Module):
         losses["mel"] = (
             FastSpeech2Loss.get_loss(
                 result["mel"],
-                target["mel"],
+                target["mel"].to(dtype=result["mel"].dtype),
                 self.l1_loss,
                 tgt_mask,
                 unsqueeze=True,

src/fastspeech2/model.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 import torch
 import torch.nn as nn
-from torch.nn.modules.transformer import TransformerEncoderLayer, TransformerEncoder
+from .torch_transformer import TransformerEncoderLayer, TransformerEncoder
 from torch.nn.utils.rnn import pad_sequence
 from third_party.stochastic_duration_predictor.sdp import StochasticDurationPredictor
 from dataset.cwt import CWT
@@ -72,7 +72,7 @@ class ConformerEncoderLayer(TransformerEncoderLayer):
                     kwargs["conv_in"],
                     kwargs["conv_in"],
                     kernel_size=kwargs["conv_kernel"][0],
-                    padding=(kwargs["conv_kernel"][0] - 1) // 2,
+                    padding='same',
                     groups=kwargs["conv_in"],
                 ),
                 nn.Conv1d(kwargs["conv_in"], kwargs["conv_filter_size"], 1),
@@ -82,7 +82,7 @@ class ConformerEncoderLayer(TransformerEncoderLayer):
                     kwargs["conv_filter_size"],
                     kwargs["conv_filter_size"],
                     kernel_size=kwargs["conv_kernel"][1],
-                    padding=(kwargs["conv_kernel"][1] - 1) // 2,
+                    padding='same',
                     groups=kwargs["conv_in"],
                 ),
                 nn.Conv1d(kwargs["conv_filter_size"], kwargs["conv_in"], 1),
@@ -92,13 +92,13 @@ class ConformerEncoderLayer(TransformerEncoderLayer):
                 kwargs["conv_in"],
                 kwargs["conv_filter_size"],
                 kernel_size=kwargs["conv_kernel"][0],
-                padding=(kwargs["conv_kernel"][0] - 1) // 2,
+                padding='same',
             )
             self.conv2 = nn.Conv1d(
                 kwargs["conv_filter_size"],
                 kwargs["conv_in"],
                 kernel_size=kwargs["conv_kernel"][1],
-                padding=(kwargs["conv_kernel"][1] - 1) // 2,
+                padding='same',
             )
 
     def forward(self, src, src_mask=None, src_key_padding_mask=None):
@@ -289,6 +289,9 @@ class VarianceAdaptor(nn.Module):
                     (torch.exp(duration_pred + 1e-9))
                 ).masked_fill(duration_pred == 0, 0)
             duration_rounded = torch.clamp(duration_rounded, min=0).int()
+            if duration_rounded.sum() == 0:
+                duration_rounded.fill_(1)
+                print("Zero duration, filling with 1, this should only happen before training")
 
         x, tgt_mask = self.length_regulator(x, duration_rounded, self.max_length)
 

src/train.py
@@ -1,7 +1,11 @@
 from argparse import ArgumentParser
 import os
 import inspect
-from re import T
+from glob import glob
+from pathlib import Path
+import json
+import hashlib
+import pickle
 
 import torch
 import torch.multiprocessing
@@ -30,6 +34,9 @@ if __name__ == "__main__":
 
     lr_monitor = LearningRateMonitor(logging_interval="step")
     callbacks = [lr_monitor]
+
+    parser.add_argument("--dataset_cache_path", type=str, default="../dataset_cache")
+    parser.add_argument("--no_cache", type=str2bool, default=False)
 
     parser.add_argument(
         "--train_target_path", type=str, nargs="+", default=["../data/train-clean-360-aligned"]
@@ -83,24 +90,71 @@ if __name__ == "__main__":
         )
 
     train_ds = []
-    for i in range(len(var_args["train_target_path"])):
-        train_ds += [LibrittsDataset(
-            target_directory=var_args["train_target_path"][i],
-            source_directory=var_args["train_source_path"][i],
-            source_url=var_args["train_source_url"][i],
-            verbose=True,
-            tmp_directory=var_args["train_tmp_path"],
-            chunk_size=10_000,
-        )]
 
-    valid_ds = LibrittsDataset(
-        target_directory=var_args["valid_target_path"],
-        source_directory=var_args["valid_source_path"],
-        source_url=var_args["valid_source_url"],
-        verbose=True,
-        tmp_directory=var_args["valid_tmp_path"],
-        chunk_size=10_000,
-    )
+    train_ds_kwargs = {
+        k.replace("train_", ""): v
+        for k, v in var_args.items()
+        if k.startswith("train_")
+    }
+
+    valid_ds_kwargs = {
+        k.replace("valid_", ""): v
+        for k, v in var_args.items()
+        if k.startswith("valid_")
+    }
+
+    if not var_args["no_cache"]:
+        Path(var_args["dataset_cache_path"]).mkdir(parents=True, exist_ok=True)
+        cache_path = Path(var_args["dataset_cache_path"])
+    else:
+        cache_path = None
+
+    for i in range(len(var_args["train_target_path"])):
+        if not var_args["no_cache"]:
+            kwargs = train_ds_kwargs
+            kwargs.update({"target_directory": var_args["train_target_path"][i]})
+            ds_hash = hashlib.md5(json.dumps(kwargs, sort_keys=True).encode('utf-8')).hexdigest()
+            cache_path_alignments = Path(var_args["dataset_cache_path"]) / f"train-alignments-{ds_hash}.pt"
+        if len(list(Path(var_args["train_target_path"][i]).rglob("**/*.TextGrid"))) == 0 or not cache_path_alignments.exists() or var_args["no_cache"]:
+            train_ds += [LibrittsDataset(
+                target_directory=var_args["train_target_path"][i],
+                source_directory=var_args["train_source_path"][i],
+                source_url=var_args["train_source_url"][i],
+                verbose=True,
+                tmp_directory=var_args["train_tmp_path"],
+                chunk_size=10_000,
+            )]
+            if not var_args["no_cache"]:
+                train_ds[-1].hash = ds_hash
+                with open(cache_path_alignments, "wb") as f:
+                    pickle.dump(train_ds[-1], f)
+        else:
+            if cache_path_alignments.exists():
+                with open(cache_path_alignments, "rb") as f:
+                    train_ds += [pickle.load(f)]
+                
+    if not var_args["no_cache"]:
+        kwargs = valid_ds_kwargs
+        kwargs.update({"target_directory": var_args["valid_target_path"]})
+        ds_hash = hashlib.md5(json.dumps(kwargs, sort_keys=True).encode('utf-8')).hexdigest()
+        cache_path_alignments = Path(var_args["dataset_cache_path"]) / f"valid-alignments-{ds_hash}.pt"
+    if len(list(Path(var_args["valid_target_path"]).rglob("**/*.TextGrid"))) == 0 or not cache_path_alignments.exists():
+        valid_ds = LibrittsDataset(
+            target_directory=var_args["valid_target_path"],
+            source_directory=var_args["valid_source_path"],
+            source_url=var_args["valid_source_url"],
+            verbose=True,
+            tmp_directory=var_args["valid_tmp_path"],
+            chunk_size=10_000,
+        )
+        if not var_args["no_cache"]:
+            valid_ds.hash = ds_hash
+            with open(cache_path_alignments, "wb") as f:
+                pickle.dump(valid_ds, f)
+    else:
+        if cache_path_alignments.exists():
+            with open(cache_path_alignments, "rb") as f:
+                valid_ds = pickle.load(f)
 
     model_args = {
         k: v
@@ -124,20 +178,14 @@ if __name__ == "__main__":
             args.from_checkpoint,
             train_ds=train_ds,
             valid_ds=valid_ds,
-            train_ds_kwargs={
-                k.replace("train_", ""): v
-                for k, v in var_args.items()
-                if k.startswith("train_")
-            },
-            valid_ds_kwargs={
-                k.replace("valid_", ""): v
-                for k, v in var_args.items()
-                if k.startswith("valid_")
-            },
+            train_ds_kwargs=train_ds_kwargs,
+            valid_ds_kwargs=valid_ds_kwargs,
             num_workers=4,
-            batch_size=args.batch_size
+            batch_size=args.batch_size,
+            cache_path=cache_path,
         )
     else:
+        model_args["cache_path"] = cache_path
         model = FastSpeech2(
             train_ds,
             valid_ds,
@@ -179,7 +227,6 @@ if __name__ == "__main__":
         callbacks=callbacks,
         default_root_dir="logs",
         logger=wandb_logger,
-        strategy=None,
     )
 
     trainer.fit(model)